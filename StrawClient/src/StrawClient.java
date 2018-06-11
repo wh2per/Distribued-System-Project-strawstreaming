d842b52a001f0a0440e76ffd7e4ef7459018a454
wh2per <93negaro@gmail.com>
2018-06-11 22:14:34 +0900

    Update StrawClient.java

--- StrawClient/src/StrawClient.java
+++ StrawClient/src/StrawClient.java
@@ -1,8 +1,3 @@
-/**
- * Created by jakeu on 2018. 6. 11..
- */
-import java.net.*;
-
 public class StrawClient {
 
     final static int port = 9999;
@@ -10,7 +5,9 @@ public class StrawClient {
     public static void main(String[] args){
         try {
             ClientThread ct = new ClientThread();
+            PlayThread pt = new PlayThread();
             ct.start();
+            pt.start();
         } catch (Exception e) {
             System.out.println(e);
         }
